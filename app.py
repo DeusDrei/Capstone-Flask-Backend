08d9122fd93225521665e7970f367842885e5557
Alain Torno <alaintorno@gmail.com>
2025-10-12 21:49:34 +0800

    app.py changes

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ load_dotenv()
 app = create_app()
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
